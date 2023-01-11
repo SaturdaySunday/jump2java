5c68657c4c969c1addb60f34bc3fb27fad33ee1f
SaturdaySunday <bangjs12@naver.com>
2023-01-11 21:08:25 +0900

    3

--- src/chap_06/_03_Return.java
+++ src/chap_06/_03_Return.java
@@ -2,9 +2,25 @@ package chap_06;
 
 public class _03_Return {
     // 호텔 전화번호
+    public static String getPhoneNumber() {
+        String phoneNumber = "02-1234-5678";
+        return phoneNumber;
+    }
     // 호텔 주소
+    public static String getAddress() {
+        return "서울";
+    }
     // 호텔 액티비티
+    public static String getActivities() {
+        return "볼링, 탁구, 노래";
+    }
     public static void main(String[] args) {
+       String contactNumber = getPhoneNumber();
+        System.out.println("전화번호 : " + contactNumber);
+
+        String address = getAddress();
+        System.out.println("주소 : " + address);
+        System.out.println("액티 : " + getActivities());
 
     }
 }
---
+++ src/chap_06/_04_ParameterAndReturn.java
@@ -0,0 +1,7 @@
+package chap_06;
+
+public class _04_ParameterAndReturn {
+    public static void main(String[] args) {
+
+    }
+}
